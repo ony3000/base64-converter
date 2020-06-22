b3b1111609a5a24d68acc88c5205558100eec864
Hyeonjong <nianelo4@gmail.com>
2020-06-23 00:53:45 +0900

    Add suite of tests

---
+++ src/index.common.test.js
@@ -0,0 +1,21 @@
+const converter = require('./index');
+const { base64Encode, base64Decode } = require('./index');
+
+const plainText = 'Hello world!';
+const base64Text = 'SGVsbG8gd29ybGQh';
+
+test('Simple encoding #1', () => {
+  expect(converter.base64Encode(plainText)).toBe(base64Text);
+});
+
+test('Simple encoding #2', () => {
+  expect(base64Encode(plainText)).toBe(base64Text);
+});
+
+test('Simple decoding #1', () => {
+  expect(converter.base64Decode(base64Text)).toBe(plainText);
+});
+
+test('Simple decoding #2', () => {
+  expect(base64Decode(base64Text)).toBe(plainText);
+});
---
+++ src/index.esm.test.js
@@ -0,0 +1,21 @@
+import converter from './index';
+import { base64Encode, base64Decode } from './index';
+
+const plainText = 'Hello world!';
+const base64Text = 'SGVsbG8gd29ybGQh';
+
+test('Simple encoding #1', () => {
+  expect(converter.base64Encode(plainText)).toBe(base64Text);
+});
+
+test('Simple encoding #2', () => {
+  expect(base64Encode(plainText)).toBe(base64Text);
+});
+
+test('Simple decoding #1', () => {
+  expect(converter.base64Decode(base64Text)).toBe(plainText);
+});
+
+test('Simple decoding #2', () => {
+  expect(base64Decode(base64Text)).toBe(plainText);
+});
