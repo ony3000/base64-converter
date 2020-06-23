be8cb3241873af6697f43d977d6ee1a500884ba6
Hyeonjong <nianelo4@gmail.com>
2020-06-23 23:21:29 +0900

    Divide webpack config

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -2,14 +2,11 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  devServer: {
-    hot: true
-  },
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, '../dist'),
     filename: 'converter.js',
     libraryTarget: 'umd',
     globalObject: 'this',
-    library: 'converter'
-  }
+    library: 'converter',
+  },
 };
