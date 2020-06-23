03931a3e8e7bc89c60a0045f6361f68a96dcdfee
Hyeonjong <nianelo4@gmail.com>
2020-06-23 22:48:18 +0900

    Modify code to pass all tests

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-export function base64Encode(input) {
+function base64Encode(input) {
   return Buffer.from(input, 'utf8').toString('base64');
 }
 
-export function base64Decode(input) {
+function base64Decode(input) {
   return Buffer.from(input, 'base64').toString('utf8');
 }
+
+module.exports = {
+  base64Encode: base64Encode,
+  base64Decode: base64Decode,
+};
