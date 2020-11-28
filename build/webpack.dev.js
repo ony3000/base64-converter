28c0bac6dd7ac16b85d89367da099c1c3261b725
Hyeonjong <nianelo4@gmail.com>
2020-11-28 21:20:45 +0900

    Move index page

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,9 +1,12 @@
+const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
   devServer: {
+    contentBase: path.join(__dirname, '../static'),
     hot: true,
+    open: true
   },
 });
