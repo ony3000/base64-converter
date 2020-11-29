dd2fbbf9cc01aa78518ccb5a45a6708567b1add0
Hyeonjong <nianelo4@gmail.com>
2020-11-29 23:47:06 +0900

    Update index page

--- static/demo.js
+++ static/demo.js
@@ -1,30 +1,72 @@
 window.addEventListener('load', function () {
   var $input = document.querySelector('#input');
   var $output = document.querySelector('#output');
-  var $encodeButton = document.querySelector('#encode-button');
-  var $decodeButton = document.querySelector('#decode-button');
-  var $clearButton = document.querySelector('#clear-button');
+  var $invalidInputHelper = document.createElement('p');
 
-  $encodeButton.addEventListener('click', function () {
-    var inputText = $input.value;
-    var outputText = converter.base64Encode(inputText);
+  $invalidInputHelper.classList.add('help', 'is-danger');
+  $invalidInputHelper.textContent = 'The input text does not seem to be in base64 format.';
 
-    $output.value = outputText;
-  });
+  function convertInput(inputText) {
+    var hash = location.hash || '#encode';
 
-  $decodeButton.addEventListener('click', function () {
-    var inputText = $input.value;
-    var outputText = converter.base64Decode(inputText);
-    var pattern = new RegExp(inputText + '=*$');
+    if (hash === '#encode') {
+      var outputText = converter.base64Encode(inputText);
 
-    if (!converter.base64Encode(outputText).match(pattern)) {
-      console.warn('The input text [' + inputText + '] does not seem to be in base64 format.');
+      $output.value = outputText;
     }
-    $output.value = outputText;
+    else if (hash === '#decode') {
+      var outputText = converter.base64Decode(inputText);
+      var pattern = new RegExp(inputText + '=*$');
+
+      if (converter.base64Encode(outputText).match(pattern)) {
+        $input.classList.remove('is-danger');
+        $output.value = outputText;
+
+        if ($invalidInputHelper.closest('div.field')) {
+          $input.closest('div.field').removeChild($invalidInputHelper);
+        }
+      }
+      else {
+        $input.classList.add('is-danger');
+        $output.value = '';
+
+        if (!$invalidInputHelper.closest('div.field')) {
+          $input.closest('div.field').appendChild($invalidInputHelper);
+        }
+      }
+    }
+  }
+
+  $input.addEventListener('input', function (event) {
+    convertInput(event.target.value);
   });
 
-  $clearButton.addEventListener('click', function () {
+  function activateTab() {
+    var $existingActivatedTab = document.querySelector('.tabs ul li.is-active');
+
+    if ($existingActivatedTab) {
+      $existingActivatedTab.classList.remove('is-active');
+    }
+
+    var hash = location.hash || '#encode';
+
+    switch (hash) {
+    case '#encode':
+    case '#decode':
+      document.querySelector(hash + '-tab').classList.add('is-active');
+      break;
+    default:
+      break;
+    }
+  }
+
+  function initialize() {
+    activateTab();
+
     $input.value = '';
     $output.value = '';
-  });
+  }
+
+  window.addEventListener('hashchange', initialize);
+  initialize();
 });
