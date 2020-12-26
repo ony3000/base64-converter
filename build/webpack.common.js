f85d94f9dc4fa4ef4d1c0291eaeb87f08475ab55
Hyeonjong <nianelo4@gmail.com>
2020-12-26 15:10:28 +0900

    Add or update configurations for TypeScript

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -1,12 +1,32 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, '../dist'),
     filename: 'converter.js',
     libraryTarget: 'umd',
     globalObject: 'this',
     library: 'converter',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+          'ts-loader',
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
   },
 };
