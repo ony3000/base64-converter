ace990105b95805a4a622cb0c9faf9549c7fa028
Hyeonjong <nianelo4@gmail.com>
2020-12-27 20:04:07 +0900

    Modify code to TypeScript syntax

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
-function base64Encode(input: string): string {
+interface ConvertingFunction {
+  (input: string): string;
+}
+
+const base64Encode: ConvertingFunction = function (input: string): string {
   return Buffer.from(input, 'utf8').toString('base64');
-}
+};
 
-function base64Decode(input: string): string {
+const base64Decode: ConvertingFunction = function (input: string): string {
   return Buffer.from(input, 'base64').toString('utf8');
-}
+};
 
-module.exports = {
-  base64Encode: base64Encode,
-  base64Decode: base64Decode,
+export = {
+  base64Encode,
+  base64Decode,
 };
