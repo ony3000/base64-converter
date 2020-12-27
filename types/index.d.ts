426ce5a4973dbd6a0e14dc4086d80e91932980e0
Hyeonjong <nianelo4@gmail.com>
2020-12-27 20:05:50 +0900

    Add type declarations

---
+++ types/index.d.ts
@@ -0,0 +1,8 @@
+interface ConvertingFunction {
+    (input: string): string;
+}
+declare const _default: {
+    base64Encode: ConvertingFunction;
+    base64Decode: ConvertingFunction;
+};
+export = _default;
