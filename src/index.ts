d3374405bbf782f86cbca08a49d2cab1ee9b5cec
Hyeonjong <nianelo4@gmail.com>
2022-01-15 00:22:15 +0900

    Omit type annotation where type inference is possible

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 type ConvertingFunction = (input: string) => string;
 
-const base64Encode: ConvertingFunction = function (input: string): string {
+const base64Encode: ConvertingFunction = function (input) {
   return Buffer.from(input, 'utf8').toString('base64');
 };
 
-const base64Decode: ConvertingFunction = function (input: string): string {
+const base64Decode: ConvertingFunction = function (input) {
   return Buffer.from(input, 'base64').toString('utf8');
 };
 
