61efc8806e5a879bdf89f25639010fb3041f7ef3
Hyeonjong <nianelo4@gmail.com>
2022-01-16 17:42:32 +0900

    Upgrade webpack related packages

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,11 +1,11 @@
 const path = require('path');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
   devServer: {
-    contentBase: path.join(__dirname, '../static'),
+    static: path.join(__dirname, '../static'),
     hot: true,
     open: true
   },
